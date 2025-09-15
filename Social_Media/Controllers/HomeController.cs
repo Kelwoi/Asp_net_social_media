2e4225b85e6ae8f56b8381fe936ab35db6eeb119
Vova Gordienko <vova0110gordienko@gmail.com>
2025-09-15 19:02:48 +0200

    First_update_design_plusforms

---
+++ Social_Media/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Social_Media.Models;
+
+namespace Social_Media.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
